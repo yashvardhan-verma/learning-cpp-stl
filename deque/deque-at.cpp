7925e6770a64fe54fea1654949efd3f95d1eb16f
Yashvardhan <iamyashvardhan6@gmail.com>
2021-01-31 21:03:45 +0530

    Added <deque> member functions files.

--- deque/deque-at.cpp
+++ deque/deque-at.cpp
@@ -5,5 +5,16 @@ using namespace std;
 
 int main()
 {
+	deque<unsigned> mydeque (10);
 	
+	for (unsigned i = 0; i < mydeque.size(); i++)
+		mydeque.at(i) = i;
+	
+	cout<<"deque contains : ";
+	for (unsigned i = 0; i < mydeque.size(); i++)
+		cout<< ' ' << mydeque.at(i);
+	
+	cout<< '\n';
+	
+	return 0;
 }
